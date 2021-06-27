64e2bd205106ce87c868452f2e9f4551c7e0487f
Lydia Yang <lydiayang92@gmail.com>
2021-06-26 21:26:45 -0700

    Route
    Added routes and several pages (Library, Playlist, Search)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+// import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Library from './pages/Library';
+import Playlist from './pages/Playlist';
+import Search from './pages/Search'
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <Router>
+      <Switch>
+
+        {/** Home Page */}
+        <Route exact path ="/">
+          <Library/>
+        </Route>
+
+        {/** Search Page */}
+        <Route path = "/search">
+          <Search/>
+        </Route>
+
+        {/** Playlist Page */}
+        <Route path = "/playlist">
+          <Playlist/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
